aa6743ea840472107aaa8b9ae0f6d2a198b5b02c
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 16:11:20 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,15 @@ end
 def last_element(array)
   return array[-1]
 end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
